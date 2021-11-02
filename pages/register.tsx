d6f400256b3300e2bb854863edb26086cb501a33
Philipp Binder <binder.philipp10@gmail.com>
2021-11-02 15:42:30 +0100

    Add functional emotion

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,4 +1,11 @@
+import { css } from '@emotion/react'; // emotion not working
 import { useState } from 'react';
+
+const formStyles = css`
+  label {
+    display: block;
+  }
+`;
 
 export default function RegisterPage() {
   const [username, setUsername] = useState('');
@@ -6,14 +13,34 @@ export default function RegisterPage() {
   return (
     <div>
       <h1> Register </h1>
-      <input
-        value={username}
-        onChange={(event) => setUsername(event.currentTarget.value)}
-      />
-      <input
-        value={password}
-        onChange={(event) => setPassword(event.currentTarget.value)}
-      />
+      <form
+        css={formStyles}
+        onSubmit={(event) => {
+          event.preventDefault(); // will prevent the default behavior on submit, instead the following code lines will be executed
+          console.log(
+            'Submitting username:',
+            username,
+            'and password:',
+            password,
+          );
+        }}
+      >
+        <label>
+          Enter your username:{' '}
+          <input
+            value={username}
+            onChange={(event) => setUsername(event.currentTarget.value)}
+          />
+        </label>
+        <label>
+          Enter your password:{' '}
+          <input
+            value={password}
+            onChange={(event) => setPassword(event.currentTarget.value)}
+          />
+        </label>
+        <button> Submit </button>
+      </form>
     </div>
   );
 }
